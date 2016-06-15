079fe6adfc3c84841977e1bfa60e8e21a641f4b5
Sam Gunaratne <samgzeit@gmail.com>
2016-06-15 16:39:07 +0100

    Add notification settings

---
+++ recipes/notifications.rb
@@ -0,0 +1,16 @@
+prefs = node['sprout']['notifications']
+
+plist_dir = "#{node['sprout']['home']}/Library/Preferences/ByHost"
+domains = Dir["#{plist_dir}/com.apple.notificationcenterui.*.plist"].map do |f|
+  domain = File.basename(f).chomp('.plist')
+  "ByHost/#{domain}"
+end
+
+domains.each do |domain|
+  osx_defaults "sets dnd mode when mirroring" do
+    domain domain
+    key 'dndMirroring'
+    boolean prefs['dndMirroring']
+    only_if { prefs['dndMirroring'] }
+  end
+end
